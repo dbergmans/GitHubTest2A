86e58168ea7e89c0961db314c64f9f2e0ff28d98
D. Bergmans <d.bergmans@rocwb.nl>
2020-04-03 13:59:23 +0200

    Added a button with an event.

--- GitHubTest2A/MainWindow.xaml.cs
+++ GitHubTest2A/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace GitHubTest2A
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
